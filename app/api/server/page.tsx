7873511880c8171511fa4ae0ae3265519fb58f35
himanshu <himanshu@nicolethemathlady.com>
2024-02-26 02:05:06 -0500

    try5

--- app/api/server/page.tsx
+++ app/api/server/page.tsx
@@ -1,5 +1,5 @@
 export default async function Page() {
-  const res = await fetch('http://localhost:3001/api/time');
+  const res = await fetch('https://black-bay-06c095410.4.azurestaticapps.net/api/time');
   let data = await res.json();
   const appVersion = process.env.NEXT_PUBLIC_APP_VERSION;
 
